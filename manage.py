63afc558d342ceeb130f7f01a037940f5fb36d53
catna <catnaer@163.com>
2017-06-03 15:38:46 +0800

    CHANGE port

--- manage.py
+++ manage.py
@@ -16,7 +16,7 @@ def make_shell_context():
 
 manager.add_command('db', MigrateCommand)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command("runserver", Server(host="127.0.0.1", port=5000))
+manager.add_command("runserver", Server(host="127.0.0.1", port=8001))
 
 
 @manager.command
